c3323dbd61f9fd9caaca708e2eda126a9f023833
Alejandjro Elizondo <alexelmtz@outlook.com>
2016-02-21 01:12:11 -0600

    AWS

---
+++ Rides/Rides/Header.h
@@ -0,0 +1,20 @@
+//
+//  Header.h
+//  Rides
+//
+//  Created by Alex Elizondo on 2/21/16.
+//  Copyright © 2016 Alex Elizondo. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
+
+#import <AWSCore/AWSCore.h>
+#import <AWSS3/AWSS3.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSSQS/AWSSQS.h>
+#import <AWSSNS/AWSSNS.h>
+#import <AWSCognito/AWSCognito.h>
