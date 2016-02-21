74dbf967e65da9f8083086d2441861432189ed74
Alejandjro Elizondo <alexelmtz@outlook.com>
2016-02-21 12:47:39 -0600

    Final commit

---
+++ Rides2/Rides2/ViewController4.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController4.swift
+//  Rides2
+//
+//  Created by Alex Elizondo on 2/21/16.
+//  Copyright © 2016 Alex Elizondo. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+class ViewController4: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
