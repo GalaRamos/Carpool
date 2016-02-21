0840d8266ae2588b4092bc3364422e7094fd897d
Alejandjro Elizondo <alexelmtz@outlook.com>
2016-02-20 19:40:57 -0600

    Cambiar todo

--- Rides/Rides/ViewController2.swift
+++ Rides/Rides/ViewController2.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  ViewController2.swift
 //  Rides
 //
 //  Created by Alex Elizondo on 2/20/16.
 //  Copyright © 2016 Alex Elizondo. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController2: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
