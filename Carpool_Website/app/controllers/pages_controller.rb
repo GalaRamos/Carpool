4eade6eb1bbbbd6494f0352b23ea154a1b200db1
Rodrigo Valencia <rod_valen_ma@hotmail.com>
2016-04-10 12:40:13 -0500

    Added the Contact page to it and added options for the user to contact us

--- Carpool_Website/app/controllers/pages_controller.rb
+++ Carpool_Website/app/controllers/pages_controller.rb
@@ -3,4 +3,6 @@ class PagesController < ApplicationController
   end
   def Help
   end
+  def Contact
+  end
 end
--- Carpool_Website/config/routes.rb
+++ Carpool_Website/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'Help' => 'pages#Help'
 
+  get 'Contact' => 'pages#Contact'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
